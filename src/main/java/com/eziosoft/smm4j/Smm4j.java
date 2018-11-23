f59d25670e78e83221c5e039409e7299311436af
jonathan <jedijon567@gmail.com>
2018-11-23 15:44:38 -0500

    write more of the basic fetching

--- src/main/java/com/eziosoft/smm4j/Smm4j.java
+++ src/main/java/com/eziosoft/smm4j/Smm4j.java
@@ -1,6 +1,15 @@
 package com.eziosoft.smm4j;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Smm4j {
+
+    private static String baseurl = "https://supermariomakerbookmark.nintendo.net/courses/";
 
     public static void main(String[] args){
         // check if arguments where provided
@@ -15,9 +24,26 @@ public class Smm4j {
     // class to get level details
     public static String[] getLevel(String id){
         String[] error = {"error"};
+        Document doc;
         // is the level id provided blank?
         if (id.equals(null)){
             return error;
         }
+        // use jsoup to grab the document
+        try {
+            doc = Jsoup.connect(baseurl + id).userAgent("Mozilla/5.0 (Debian Stretch) Gecko/20100101 Firefox/63.0").get();
+        } catch (IOException e){
+            System.out.println("Error!");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return error;
+        }
+        // is there an error?
+        if (doc.select("div.error-description").equals("The page could not be found.")){
+            String[] out = {"404"};
+            return out;
+        }
+        // get just the course card
+        Elements card = doc.select("div.course-card");
     }
 }
