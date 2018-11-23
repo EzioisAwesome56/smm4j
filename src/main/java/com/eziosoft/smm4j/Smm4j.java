decafecb1699dc1d500d4ca36220a957d6d7ed3b
jonathan <jedijon567@gmail.com>
2018-11-23 15:18:53 -0500

    start writing the api

--- src/main/java/com/eziosoft/smm4j/Smm4j.java
+++ src/main/java/com/eziosoft/smm4j/Smm4j.java
@@ -1,4 +1,19 @@
 package com.eziosoft.smm4j;
 
 public class Smm4j {
+
+    public static void main(String[] args){
+        // check if arguments where provided
+        if (args.length > 0){
+            // then parse args
+            if (args[0].equals("-cli")){
+                System.out.println("Command line mode Selected!");
+            }
+        }
+    }
+
+    // class to get level details
+    public static String[] getLevel(String id){
+
+    }
 }
